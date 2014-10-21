0b1b01880165583dfb0059fc046eb708706004f7
xzhangpeijin <xzhangpeijin@gmail.com>
2014-10-21 04:49:01 -0400

    Reduction update

---
+++ src/main/utils/ReduceGraph.java
@@ -0,0 +1,15 @@
+package main.utils;
+
+
+/**
+ * Tries to reduce a graph by removing nodes that can't be part of the longest path
+ * 
+ * @author Peijin Zhang
+ */
+public class ReduceGraph
+{
+  public static Graph reduceGraph(Graph graph) {
+    
+    return null;
+  }
+}
