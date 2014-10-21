8233514f3245b97a3a3c9c7f16057eed344e46f3
xzhangpeijin <xzhangpeijin@gmail.com>
2014-10-21 16:10:03 -0400

    Walker refactor

--- src/main/utils/RandomWalker.java
+++ src/main/utils/RandomWalker.java
@@ -1,13 +1,8 @@
 package main.utils;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -17,31 +12,17 @@ import java.util.concurrent.locks.Lock;
  * 
  * @author Peijin Zhang
  */
-public class RandomWalk implements Runnable {
-  private final Graph graph;
-  private final List<Integer> initstates;
-  private final Random random;
-
+public class RandomWalker extends Walker {
   private final Set<Integer>[] fReachable;
   private final Set<Integer>[] bReachable;
 
-  private final String filename;
-  private final AtomicInteger maxLength;
-  private final Lock fileLock;
-
-  public RandomWalk(Graph graph, List<Integer> initstates,
+  public RandomWalker(Graph graph, List<Integer> initstates,
       Set<Integer>[] fReachable, Set<Integer>[] bReachable,  String filename,
       AtomicInteger maxLength, Lock fileLock) {
-    this.graph = graph;
-    this.initstates = initstates;
-    this.random = new Random(System.nanoTime());
+    super(graph, initstates, filename, maxLength, fileLock);
 
     this.fReachable = fReachable;
     this.bReachable = bReachable;
-
-    this.filename = filename;
-    this.maxLength = maxLength;
-    this.fileLock = fileLock;
   }
 
   /**
@@ -106,66 +87,6 @@ public class RandomWalk implements Runnable {
     public Candidate(int node, boolean forward) {
       this.node = node;
       this.forward = forward;
-    }
-
-  }
-
-  /*
-   * Result file format
-   * 
-   * Paths come in 3 lines: Line 1: Length of the path Line 2: Seed used to
-   * generate the path Line 3: Space separated vertices for the path
-   * 
-   * Paths in the file are guaranteed to be in strictly increasing path length
-   */
-
-  /**
-   * Writes path to file
-   */
-  public void writeResult(List<Integer> path) {
-    fileLock.lock();
-    
-    // Check again in case it changed
-    if (path.size() > maxLength.get()) {
-      try {
-        System.out.println(path.size());
-
-        PrintWriter out = new PrintWriter(new FileWriter(new File(filename),
-            true));
-        out.println("Path Length: " + path.size());
-
-        StringBuffer buf = new StringBuffer();
-        for (int vertex : path) {
-          buf.append(vertex);
-          buf.append(" ");
-        }
-        out.println(buf.toString().trim());
-
-        out.flush();
-        out.close();
-
-        maxLength.set(path.size());
-      } catch (IOException e) {
-        System.err.println("Error writing to " + filename);
-        e.printStackTrace();
-      }
-    }
-
-    fileLock.unlock();
-  }
-
-  @Override
-  public void run() {
-    List<Integer> path = new ArrayList<Integer>();
-    while (true) {
-      walkPath(path);
-      
-      // Check with atomic integer so no context switch required
-      if (path.size() > maxLength.get()) {
-        writeResult(path);
-      }
-
-      path.clear();
     }
   }
 }
